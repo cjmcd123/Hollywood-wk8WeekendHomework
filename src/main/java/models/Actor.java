93126b3021dd011ab9b099333bdc3bfc61e37b60
cjmcd123 <cj_mcdowall@btinternet.com>
2018-07-06 12:43:55 +0100

    basic actor class

--- src/main/java/models/Actor.java
+++ src/main/java/models/Actor.java
@@ -1,4 +1,91 @@
 package models;
 
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "actors")
 public class Actor {
+
+    private int id;
+    private String name;
+    private int age;
+    private int price;
+    private int money;
+    private List<Film> films;
+
+    public Actor(){
+
+    }
+
+    public Actor(String name, int age, int price) {
+        this.name = name;
+        this.age = age;
+        this.price = price;
+        this.films = new ArrayList<Film>();
+        this.money = 0;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    @Column(name = "age")
+    public int getAge() {
+        return age;
+    }
+
+    @Column(name = "price")
+    public int getPrice() {
+        return price;
+    }
+
+    @Column(name = "money")
+    public int getMoney(){
+        return money;
+    }
+
+    @ManyToMany
+    @JoinTable(name = "actor_film",
+            joinColumns = {@JoinColumn(name = "film_id", nullable = false, updatable = false)},
+            inverseJoinColumns = {@JoinColumn(name = "actor_id", nullable = false, updatable = false)})
+    @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
+    public List<Film> getFilms() {
+        return films;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public void addFilms(Film film) {
+        this.films.add(film);
+    }
 }
--- src/test/java/ActorTest.java
+++ src/test/java/ActorTest.java
@@ -1,2 +1,74 @@
+import models.Actor;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.persistence.Table;
+
+import static org.junit.Assert.assertEquals;
+
 public class ActorTest {
+
+    Actor actor;
+
+    @Before
+    public void setUp(){
+        actor = new Actor("Benedict Cumberbatch", 41, 1000);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Benedict Cumberbatch", actor.getName());
+    }
+
+    @Test
+    public void setName(){
+        actor.setName("Benedict Cumberbatch2");
+        assertEquals("Benedict Cumberbatch2", actor.getName());
+    }
+
+    @Test
+    public void hasAge(){
+        assertEquals(41, actor.getAge());
+    }
+
+    @Test
+    public void setAge(){
+        actor.setAge(21);
+        assertEquals(21, actor.getAge());
+    }
+
+    @Test
+    public void hasPrice(){
+        assertEquals(1000, actor.getPrice());
+    }
+
+    @Test
+    public void setPrice(){
+        actor.setPrice(1200);
+        assertEquals(1200, actor.getPrice());
+    }
+
+    @Test
+    public void moneyStartsEmpty(){
+        assertEquals(0, actor.getMoney());
+    }
+
+    @Test
+    public void setMoney(){
+        actor.setMoney(10);
+        assertEquals(10, actor.getMoney());
+    }
+
+    @Test
+    public void moviesStartsEmpty(){
+        assertEquals(0, actor.getFilms().size());
+    }
+
+//    @Test
+//    public void addMovie(){
+//        actor.addFilm(movie);
+//        assertEquals(1, actor.getFilms().size());
+//    }
+//    commented out undtil movie class created
+
 }
