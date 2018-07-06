f29bafac6623ba3b44ba2bc27be4fff5cdf64c73
cjmcd123 <cj_mcdowall@btinternet.com>
2018-07-06 12:51:27 +0100

    basic director class

--- src/main/java/models/Director.java
+++ src/main/java/models/Director.java
@@ -1,4 +1,67 @@
 package models;
 
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Director {
+
+    private int id;
+    private String name;
+    private int age;
+    private List<Film> films;
+
+    public Director(){
+
+    }
+
+    public Director(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.films = new ArrayList<Film>();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    @Column(name = "age")
+    public int getAge() {
+        return age;
+    }
+
+    @ManyToMany
+    @JoinTable(name = "director_film",
+            joinColumns = {@JoinColumn(name = "film_id", nullable = false, updatable = false)},
+            inverseJoinColumns = {@JoinColumn(name = "director_id", nullable = false, updatable = false)})
+    @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
+    public List<Film> getFilms() {
+        return films;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setFilms(List<Film> films) {
+        this.films = films;
+    }
 }
--- src/test/java/DirectorTest.java
+++ src/test/java/DirectorTest.java
@@ -1,2 +1,50 @@
+import models.Director;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DirectorTest {
+
+    Director director;
+
+    @Before
+    public void setUp(){
+        director = new Director("Scott Derrickson", 51);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Scott Derrickson", director.getName());
+    }
+
+    @Test
+    public void setName(){
+        director.setName("Scott Derrickson2");
+        assertEquals("Scott Derrickson2", director.getName());
+    }
+
+    @Test
+    public void hasAge(){
+        assertEquals(51, director.getAge());
+    }
+
+    @Test
+    public void setAge(){
+        director.setAge(21);
+        assertEquals(21, director.getAge());
+    }
+
+    @Test
+    public void moviesStartsEmpty(){
+        assertEquals(0, director.getFilms().size());
+    }
+
+//    @Test
+//    public void addMovie(){
+//        actor.addFilm(movie);
+//        assertEquals(1, actor.getFilms().size());
+//    }
+//    commented out undtil movie class created
+
 }
